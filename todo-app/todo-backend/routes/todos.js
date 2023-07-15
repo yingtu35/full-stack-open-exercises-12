054210753044c69e1eabac8ea93fd5334edcb271
Ying Tu <tuying@Ying-MBA.local>
2023-07-15 21:04:07 +0800

    exercise 12.12 docker and persistant data with mongo and redis

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,23 +1,39 @@
-const express = require('express');
+const express = require('express')
 const { Todo } = require('../mongo')
-const router = express.Router();
+const router = express.Router()
+const redis = require('../redis')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
+
+router.get('/statistic', async (_, res) => {
+  const added_todos = await redis.getAsync('added_todos')
+  if (added_todos) {
+    res.send({ added_todos })
+  } else {
+    res.send({ 'added_todos': "0"})
+  }
+})
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  const added_todos = await redis.getAsync('added_todos')
+  if (added_todos) {
+    await redis.setAsync('added_todos', Number(added_todos)+1)
+  } else {
+    await redis.setAsync('added_todos', 1)    
+  }
+  res.send(todo)
+})
 
-const singleRouter = express.Router();
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
@@ -29,21 +45,29 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  res.send(req.todo)
+})
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  const update = {
+    text: req.body.text,
+    done: req.body.done,
+  }
+  const options = {
+    new: true,
+    // runValidators: true
+  }
+  const newTodo = await Todo.findByIdAndUpdate(req.todo._id, update, options)
+  res.send(newTodo)
+})
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
